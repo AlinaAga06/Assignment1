fac0b7e9551ab7129e9a290335d812ea9f37f8ef
AlinaAga06 <141228720+AlinaAga06@users.noreply.github.com>
2023-08-15 18:29:03 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.js';
+import Todo from './Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
+
+
